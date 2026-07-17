231244062a3bb683a467d972215fe828647b8d58
Fakhriel Yusmana Shiddiq <fakhrielyusmanashiddiq@gmail.com>
2026-07-17 22:52:11 +0700

    Delete dashboard directory

--- dashboard/src/lib/escapeHtml.ts
+++
@@ -1,9 +0,0 @@
-export function escapeHtml(value: unknown): string {
-  if (value === null || value === undefined) return "";
-  return String(value)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
-}
--- dashboard/src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    showToast?: (msg: string, type?: "success" | "error" | "info") => void;
-  }
-}
-
-export {};
