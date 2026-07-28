548638d76e42ce5d82b1902e499b2fc910634963
Fakhriel Yusmana Shiddiq <fakhrielyusmanashiddiq@gmail.com>
2026-07-28 17:09:56 +0700

    Delete backend directory

--- backend/src/modules-owner/owner-attendance/owner-attendance.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { teamHandler } = require('./owner-attendance.controller');
-
-const router = express.Router();
-
-router.get('/team', teamHandler);
-
-module.exports = router;
--- backend/src/utils/escapeLike.js
+++
@@ -1,6 +0,0 @@
-function escapeLike(value) {
-  if (value === null || value === undefined) return '';
-  return String(value).replace(/[\\%_]/g, (char) => `\\${char}`);
-}
-
-module.exports = { escapeLike };
